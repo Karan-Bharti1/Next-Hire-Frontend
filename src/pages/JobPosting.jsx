56572aae3e716463dd54a33066b61b04e1bc3e66
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-08 14:29:46 +0530

    feat: displayed job postings data and added search  bar in order to search job by title

--- src/pages/JobPosting.jsx
+++ src/pages/JobPosting.jsx
@@ -1,8 +1,51 @@
+import { useEffect, useState } from "react"
 import Header from "../components/Header"
+import { getJobDataUrl } from "../urls"
+import useFetch from "../useFetch"
+import { Link } from "react-router-dom"
 
 const JobPosting=()=>{
+    const [jobsData,setJobData]=useState([])
+    const[searchTerm,setSearchterm]=useState("")
+    const {data,loading,error}=useFetch(getJobDataUrl)
+    useEffect(()=>{
+        if(Array.isArray(data)){
+            setJobData(data)
+        }else setJobData([])
+    },[data])
+    const filteredData=jobsData?.filter(job=>{
+const matchesSearchTerm=searchTerm===""||job.title.toLowerCase().includes(searchTerm.toLowerCase())
+return matchesSearchTerm
+    })
+    const displayData = filteredData.map((job) => (
+        <div className="col-md-4 my-2" key={job._id}>
+          <div className="card p-3">
+            <h3>{job.title}</h3>
+            <p><strong>Company Name:</strong> {job.companyName}</p>
+            <p><strong>Location:</strong> {job.location}</p>
+            <p><strong>Job Type:</strong> {job.type}</p>
+            <div>
+            <Link className="btn btn-primary" to={`/viewjob/${job._id}`}>See Details</Link>
+            <button className="btn btn-danger mx-3">Delete</button>
+            </div>
+          </div>
+        </div>
+      ));
     return(<>
     <Header/>
+    <main className="container">
+      
+<input type="text" placeholder="Search job by title..." onChange={e=>setSearchterm(e.target.value)} className="form-control my-3"/>
+<h2> All Jobs</h2>
+{loading && (<div className="d-flex align-items-center">
+  <strong>Loading...</strong>
+  <div className="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+</div>)}
+{error && <p>{error}</p>}
+<div className="row my-3">
+{displayData}
+</div>
+    </main>
     </>)
 }
 export default JobPosting
--- src/urls.js
+++ src/urls.js
@@ -1,1 +1,2 @@
 export const introImgUrl='https://images.pexels.com/photos/380768/pexels-photo-380768.jpeg?auto=compress&cs=tinysrgb&w=860&h=550&dpr=2'
+export const getJobDataUrl="https://next-hire-backend-ochre.vercel.app/"
